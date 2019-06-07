796d0104ef7ea405891434fb0b9220735fc55425
harmeet <harmeet2harry@gmail.com>
2019-06-07 23:06:10 +0530

    scond commit

--- server.js
+++ server.js
@@ -3,9 +3,10 @@
 const express = require('express');
 const  app = express();
 app.listen(process.env.PORT || 3000);
+const favicon = require('serve-favicon');
 const path = require('path');
 app.use(express.static(__dirname + '/dist'));
-app.use(favicon(__dirname + '/dist/favicon.ico'));
+app.use(favicon(__dirname + '/dist/PersonListFilter/favicon.ico'));
 
 
 app.get('/*', (req, res) => {
