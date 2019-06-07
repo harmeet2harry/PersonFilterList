a7b3ef68bd17e7b077062f9c51cf152c81b4f630
harmeet <harmeet2harry@gmail.com>
2019-06-07 23:13:18 +0530

    first

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ app.listen(process.env.PORT || 3000);
 const favicon = require('serve-favicon');
 const path = require('path');
 app.use(express.static(__dirname + '/dist'));
-app.use(favicon(__dirname + '/dist/PersonListFilter/favicon.ico'));
 
 
 app.get('/*', (req, res) => {
